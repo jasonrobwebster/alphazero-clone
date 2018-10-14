3fef31dfb37620c289c5e79a2b6f5dbe63becb6c
jasonrobwebster <jasonrobwebster@gmail.com>
2018-10-14 22:24:23 +0200

    minor tweaks to main

--- main.py
+++ main.py
@@ -4,21 +4,21 @@ from montecarlo import MCTS, MCTSNet
 from games import Chess, TicTacToe, Othello
 from players import NetPlayer
 from coach import NetCoach
-from nnet.keras import AlphaZeroNet
+from nnet.pytorch import AlphaZeroNet
 
 #game = Chess()
 #game = TicTacToe(3)
 game = Othello(6)
 print(game.board())
 print()
-net = AlphaZeroNet(game, blocks=10, epochs=10, save_path='./models/keras/othello2.model')
+net = AlphaZeroNet(game, blocks=10, epochs=10, save_path='./models/pytorch/othello.model')
 net.build()
 player = NetPlayer(game, net)
-mcts = MCTSNet(game, player=player, episodes=100, exploration=0.2)
+mcts = MCTSNet(game, player=player, episodes=100, exploration=0.7)
 #mcts = MCTS(game, episodes=100)
 max_moves = 100
 
-coach = NetCoach(mcts, net, episodes=100, buffer=50, max_moves=max_moves, train_time=14*60*60, prop_thresh=12, train_kw={'epochs': 10})
+coach = NetCoach(mcts, net, episodes=100, buffer=10, max_moves=max_moves, train_time=64*60*60, prop_thresh=12, train_kw={'epochs': 10})
 #coach = NetCoach(game, player, max_moves=10, episodes=1, iterations=2, buffer=2, train_time=300)
 coach.train()
 
